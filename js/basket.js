f24b42495e83c0cd58360e24a79bce627c9649e1
AynazFlex <jkz1713x2@gmail.com>
2022-12-09 03:31:24 +0300

    add products checkbox js logic

--- js/basket.js
+++ js/basket.js
@@ -5,6 +5,17 @@ const stcokContainer = stock.querySelector('.stock__container')
 const stcokDisableContainer = stockDisable.querySelector('.stock__container')
 const checkbox = stock.querySelector('.stock__checkbox')
 const stockInfo = stock.querySelector('.stock__info-in-dis')
+const checkboxAll = stock.querySelector('#selectAll')
+const products = stcokContainer.querySelectorAll('.product')
+const checkboxes = Array.from(products).map(elem => elem.querySelector('.product__img-select>input'))
+
+checkboxes.forEach(elem => {
+    elem.addEventListener('change', (e) => {
+        if(!e.target.checked) {
+            checkboxAll.checked = false
+        }
+    })
+})
 
 
 stockButton.addEventListener('click', () => {
@@ -18,4 +29,12 @@ stockButton.addEventListener('click', () => {
 stockDisableButton.addEventListener('click', () => {
     stcokDisableContainer.classList.toggle('stock__container-disabled')
     stockDisableButton.classList.toggle('stock__button-rotate')
+})
+
+checkboxAll.addEventListener('change', (e) => {
+    if(e.target.checked) {
+        checkboxes.forEach(item => item.checked = true)
+    } else {
+        checkboxes.forEach(item => item.checked = false)
+    }
 })
